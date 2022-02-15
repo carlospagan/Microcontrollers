d7fcdf1c9bceb38d709f3bb113d08d63b9926ef3
Carlos Eduardo Pagan <92696898+carlospagan@users.noreply.github.com>
2022-02-15 14:54:31 -0300

    Update main.c

--- Lista 04/ex04/exemplo01/Core/Src/main.c
+++ Lista 04/ex04/exemplo01/Core/Src/main.c
@@ -113,12 +113,12 @@ int main(void)
 	  potsoma += HAL_ADC_GetValue(&hadc1);
 	  i++;
 
-	  if (i >= 50){
+	  if (i >= 50){ // 10
 		  potavg = potsoma/i;
 		  voltAvg = 3.3/4095.0 * potavg;
 		  i = 0;
 		  potsoma = 0;
-	  }
+	  } 
 
 	  volt = 3.3 / 4095.0 * pot;
 
