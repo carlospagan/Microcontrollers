98d338fc13f4eb924f6234733913ff6e57ed8e6e
Carlos Eduardo Pagan <92696898+carlospagan@users.noreply.github.com>
2022-02-15 14:38:39 -0300

    Update main.c

--- Lista 04/ex09/ex09-lista04/Core/Src/main.c
+++ Lista 04/ex09/ex09-lista04/Core/Src/main.c
@@ -1,3 +1,4 @@
+<div align="center">
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
@@ -280,4 +281,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
+</div>
